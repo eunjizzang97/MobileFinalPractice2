030543f29db4ef5ecffed8f2c93af897ecf0aacb
eunjizzang97 <eunjizzang97>
2017-11-29 21:51:40 +0900

    1129 PM 9:51 Tab Activity

---
+++ app/src/main/java/com/example/hywoman/tabbedexample_ej/Tab2Chat.java
@@ -0,0 +1,20 @@
+package com.example.hywoman.tabbedexample_ej;
+
+/**
+ * Created by hywoman on 2017-11-29.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2Chat extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab2chat, container, false);
+        return rootView;
+    }
+}
